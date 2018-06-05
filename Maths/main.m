cb27b6d72b3234eafaaefc6f61189a77842144ca
Ahmad Hobby <ahmad.hobby@gmail.com>
2018-06-05 13:03:59 -0400

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Ahmad Hobby on 2018-06-05.
+//  Copyright © 2018 Ahmad Hobby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
